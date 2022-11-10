bef7f62472f4cb76400e163bcdd8b514d817f7c3
MesaZapata <mesazapatasamuel@gmail.com>
2022-11-10 10:54:40 -0500

    domain

---
+++ src/app/task/infrastructure/presentation/ui/components/task-list/task-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TaskEntity } from '../../../../../domain/entities/task.entity';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
+})
+export class TaskListComponent {
+  
+
+  constructor() {
+  
+ };
+
+}
